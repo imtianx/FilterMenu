4ad1d4328ee8ea2d44050dccc08581adbb93071b
imtianx <imtianx@gmail.com>
2018-05-10 21:16:35 +0800

    add item data class

--- filtermenu/src/main/java/cn/imtianx/filtermenu/FilterMenuListAdapter.kt
+++ filtermenu/src/main/java/cn/imtianx/filtermenu/FilterMenuListAdapter.kt
@@ -12,9 +12,9 @@ import android.widget.TextView
  *</pre>
  *
  * @author 奚岩
- * @date 2018/5/5 下午12:50
+ * @date 2018/5/5 下午2:50
  */
-class FilterMenuListAdapter(private val layoutId: Int = R.layout.item_menu_list, private val data: List<String>) : BaseAdapter() {
+class FilterMenuListAdapter(private val layoutId: Int = R.layout.item_menu_list, private val data: List<MenuItemData>) : BaseAdapter() {
     private var checkItemPosition = 0
 
     fun setCheckItem(position: Int) {
@@ -45,7 +45,7 @@ class FilterMenuListAdapter(private val layoutId: Int = R.layout.item_menu_list,
         } else {
             viewHolder = view.tag as ViewHolder
         }
-        viewHolder.mtvName.text = data[position]
+        viewHolder.mtvName.text = data[position].text
 
         if (checkItemPosition != -1) {
             if (checkItemPosition == position) {
@@ -59,7 +59,7 @@ class FilterMenuListAdapter(private val layoutId: Int = R.layout.item_menu_list,
     }
 
     internal class ViewHolder(itemView: View) {
-        var mtvName: TextView = itemView.findViewById<View>(R.id.tv_menu_item_name) as TextView
+        var mtvName: TextView = itemView.findViewById(R.id.tv_menu_item_name) as TextView
 
     }
 }
---
+++ filtermenu/src/main/java/cn/imtianx/filtermenu/MenuItemData.kt
@@ -0,0 +1,10 @@
+package cn.imtianx.filtermenu
+
+/**
+ * <pre>
+ *     @desc:
+ * </pre>
+ * @author 奚岩
+ * @date 2018/5/5 下午2:47
+ */
+data class MenuItemData(var value: String, var text : String)
